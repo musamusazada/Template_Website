fe7e5e485c80a68fec7e46ffe3564b522b393299
Musa Musazada <musaam@code.edu.az>
2021-12-29 14:55:48 +0400

    non-responsive version

--- GT-Template/src/index.js
+++ GT-Template/src/index.js
@@ -4,11 +4,14 @@
 window.onload = () => {
   //Navigation Menu Handling
   handleNavMenu();
+  //Navigation Sub=menu Handling
   handleSubMenu();
   //Handling Main Slider Background
   handleMainSlider();
+
+  //Main Slider changing background on hover
   function handleMainSlider() {
-    const sliderItems = document.querySelectorAll(".swiper-slide");
+    const sliderItems = document.querySelectorAll(".main-slider .swiper-slide");
     const mainSlider = document.querySelector(".main-slider");
     sliderItems.forEach((el) => {
       el.addEventListener("mouseover", () => {
@@ -19,18 +22,27 @@ window.onload = () => {
       });
     });
   }
+  //Opening Navigation Menus
   function handleNavMenu() {
     const navLinks = document.querySelectorAll(".menu-act");
     const menus = document.querySelectorAll(".menu");
     navLinks.forEach((el) => {
-      el.addEventListener("mouseover", () => {
+      el.addEventListener("mouseover", (e) => {
+        e.stopPropagation();
+
         menus.forEach((el) => {
           el.style.height = "";
           el.style.overflow = "hidden";
         });
-        el.nextElementSibling.style.height =
-          el.nextElementSibling.scrollHeight + "px";
+
         el.nextElementSibling.style.overflow = "visible";
+        if (el.nextElementSibling.scrollHeight < 447) {
+          el.nextElementSibling.style.height = "447px";
+        } else {
+          el.nextElementSibling.style.height =
+            el.nextElementSibling.scrollHeight + "px";
+        }
+        console.log(el.nextElementSibling.scrollHeight);
         el.nextElementSibling.firstElementChild.nextElementSibling.classList.remove(
           "d-none"
         );
@@ -43,6 +55,7 @@ window.onload = () => {
       );
     });
   }
+  //Opening Navigation submenus
   function handleSubMenu() {
     const subActs = document.querySelectorAll(".sub-act");
     const allMenus = document.querySelectorAll(".sub-menu");
@@ -62,11 +75,36 @@ window.onload = () => {
 $(document).ready(function () {
   let swiper = new Swiper(".main-slider", {
     slidesPerView: 4,
-    spaceBetween: 3,
+    spaceBetween: 2,
     loop: true,
     autoplay: {
       delay: 4500,
       disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-c-button-next",
+      prevEl: ".swiper-c-button-prev",
+    },
+  });
+  swiper.on("slideChange", function () {
+    console.log("slide changed");
+    const active = document.querySelector(".main-slider .swiper-slide-active");
+    const mainSlider = document.querySelector(".main-slider");
+    const imgIndex = active.getAttribute("data-swiper-slide-index");
+    mainSlider.style.backgroundImage = `url('./assets/img/main-slider-imgs/slider-img-${imgIndex}.jpg')`;
+  });
+
+  let articleSwiper = new Swiper(".article-swiper", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    loop: true,
+    autoplay: {
+      delay: 9000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-custom-button-next",
+      prevEl: ".swiper-custom-button-prev",
     },
   });
 });
